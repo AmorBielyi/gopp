598e787ab19a964f426e927b57a1b632b6e49926
Roman Bielyi <romanbielyi.amor@gmail.com>
2020-08-05 19:17:51 +0300

    add builtin type rule to var decl; all went ok with source

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,10 +6,11 @@ token_type token;
 void parse_grammar();
 void parse_import_decl_without_alias();
 void parse_import_decl_with_alias();
-void parse_recc_imports_decl_scope();
+void parse_import_decl_list();
 void parse_var_decl_name_list();
 void parse_var_decl_expr_list();
 void parse_var_decl_expr();
+void parse_builtin_type();
 
 /* PACKAGE RULE BEGIN */
 void parse_top_package_stmt()
@@ -27,7 +28,7 @@ void parse_top_package_stmt()
 
 int from_scope = 0;
 /* IMPORTS RULE BEGIN */
-void parse_top_imports_decl()
+void parse_top_import_decl()
 {
     if (from_scope != 1){
         token = gpplex();
@@ -45,7 +46,7 @@ void parse_top_imports_decl()
         case tk_LPAREN:
             token = gpplex();
             from_scope = 1;
-            parse_recc_imports_decl_scope();
+            parse_import_decl_list();
             from_scope = 0;
             if (token == tk_RPAREN){
                  token = gpplex();
@@ -57,9 +58,9 @@ void parse_top_imports_decl()
         
 }
 
-void parse_recc_imports_decl_scope(){
+void parse_import_decl_list(){
     do {
-         parse_top_imports_decl();
+         parse_top_import_decl();
     }while(token == tk_IDENT || token == tk_STRINGLIT);
 }
 
@@ -84,7 +85,7 @@ void parse_import_decl_with_alias(char *semantic_value_alias_name)
         
 }
 /* IMPORTS RULE END */
-int is_var_name_list_end = 0;
+
 
 /* VAR RULE BEGIN */
 void parse_var_decl_name(){
@@ -92,10 +93,15 @@ void parse_var_decl_name(){
         token = gpplex();
     }
     switch(token){
-        if (is_var_name_list_end == 0){
+        //if (is_var_name_list_end == 0){
             case tk_IDENT:
              printf("var: name '%s' ", get_queued_semantic_value());
              token = gpplex();
+             parse_builtin_type();
+            //  if (token == tk_T_INT){
+            //      printf("var: type (builtin) 'int' ");
+            //      token = gpplex();
+            //  }
              if(token == tk_IDENT){
                  printf("var: type '%s' ", get_queued_semantic_value());
                  token = gpplex();
@@ -109,7 +115,7 @@ void parse_var_decl_name(){
                
                 from_scope = 0; // or before parse_var_decl_expr(); ??? line up
             } 
-        }
+        //}
         
     }
 
@@ -120,7 +126,7 @@ void parse_var_decl_expr(){
         case tk_IDENT:
         case tk_STRINGLIT:
         case tk_NUM:
-            printf("var: value '%s, '", get_queued_semantic_value());
+            printf("var: value '%s'", get_queued_semantic_value());
             token = gpplex();
             if (token == tk_COMMA){
                 token = gpplex();
@@ -141,17 +147,12 @@ void parse_var_decl_name_list(){
     do{
         parse_var_decl_name();
     }while(token == tk_IDENT || token == tk_COMMA);
-    is_var_name_list_end = 1;
+   // is_var_name_list_end = 1;
 }
 
 void parse_top_vars_decl(){
     parse_var_decl_name();
-   // printf("tok: %u", token);
-    // if (token == tk_IDENT){
-    //     printf("var: type '%s' ", get_queued_semantic_value());
-    //     token = gpplex();
-    // }
-  //  printf("tok: %u", token);
+
     if (token == tk_ASSIGN){
         token = gpplex();
         parse_var_decl_expr();
@@ -162,7 +163,87 @@ void parse_top_vars_decl(){
 }
 /* VAR RULE END */
 
+/*SPECIAL RULE BUILTIN TYPE BEGIN*/
+void parse_builtin_type(){
+   
+     
 
+    switch(token){
+        //     break;
+        case tk_T_BOOL:
+            printf("var: type (builtin) 'bool'");
+            token = gpplex();
+        case tk_T_BYTE:
+            printf("var: type (builtin) 'byte'");
+            token = gpplex();
+            break;
+        case tk_T_COMPLEX128:
+            printf("var: type (builtin) 'complex128'");
+            token = gpplex();
+            break;
+        case tk_T_COMPLEX64:
+            printf("var: type (builtin) 'complex64'");
+            token = gpplex();
+            break;
+        case tk_T_FLOAT32:
+            printf("var type (builtin) 'float32'");
+            token = gpplex();
+            break;
+        case tk_T_INT16:
+            printf("var: type (builtin) 'int16'");
+            token = gpplex();
+            break;
+        case tk_T_INT32:
+            printf("var: type (builtin) 'int32'");
+            token = gpplex();
+            break;
+        case tk_T_INT64:
+            printf("var: type (builtin) 'int64'");
+            token = gpplex();
+            break;
+        case tk_T_INT8:
+            printf("var: type (builtin) 'int8'");
+            token = gpplex();
+            break;
+        case tk_T_INT:
+            printf("var: type (builtin) 'int'");
+            token = gpplex();
+            break;
+        case tk_T_RUNE:
+            printf("var: type (builtin) 'rune'");
+            token = gpplex();
+            break;
+        case tk_T_STRING:
+            printf("var: type (builtin) 'string'");
+            token = gpplex();
+            break;
+        case tk_T_UINT16:
+            printf("var: type (builtin) 'uint16'");
+            token = gpplex();
+            break;
+        case tk_T_UINT32:
+            printf("var: type (builtin) 'uint32'");
+            token = gpplex();
+            break;
+        case tk_T_UINT64:
+            printf("var: type (builtin) 'uint64'");
+            token = gpplex();
+            break;
+        case tk_T_UINT8:
+            printf("var: type (builtin) 'uint8'");
+            token = gpplex();
+            break;
+        case tk_T_UINT:
+            printf("var: type (builtin) 'uint'");
+            token = gpplex();
+            break;
+        case tk_T_UINTPTR:
+            printf("var: type (builtin) 'uintptr'");
+            token = gpplex();
+            break;
+    } 
+}
+/*SPECIAL RULE BUILTIN TYPE END*/
 
 void parse_grammar()
 {
@@ -173,7 +254,7 @@ void parse_grammar()
             break;
         case tk_IMPORT:
         case tk_GOIMPORT:
-            parse_top_imports_decl();
+            parse_top_import_decl();
             parse_grammar();
             break;
         /* TEST RULE; BEGIN THIS */
