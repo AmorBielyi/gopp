d9aa95d5f920a7c28cacb221a5e4e3dd21787fe8
Roman Bielyi <romanbielyi.amor@gmail.com>
2020-08-05 04:11:25 +0300

    refactor rule for import decl: add test rule for better undestanding and as start point for next day

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@ token_type token;
 void parse_grammar();
 void parse_import_decl_without_alias();
 void parse_import_decl_with_alias();
-void parse_imports_decl_scope();
+void parse_recc_imports_decl_scope();
 
 /* PACKAGE RULE BEGIN */
 void parse_package_stmt()
@@ -42,9 +42,7 @@ void parse_imports_decl()
         case tk_LPAREN:
             token = gpplex();
             from_scope = 1;
-            do {
-                parse_imports_decl_scope();
-            }while(token == tk_IDENT || token == tk_STRINGLIT) ;
+            parse_recc_imports_decl_scope();
             from_scope = 0;
             if (token == tk_RPAREN){
                  token = gpplex();
@@ -56,9 +54,10 @@ void parse_imports_decl()
         
 }
 
-void parse_imports_decl_scope(){
-    
-    parse_imports_decl();
+void parse_recc_imports_decl_scope(){
+    do {
+         parse_imports_decl();
+    }while(token == tk_IDENT || token == tk_STRINGLIT);
 }
 
 void parse_import_decl_without_alias(char* semantic_value_source_name)
@@ -94,6 +93,12 @@ void parse_grammar()
         case tk_IMPORT:
         case tk_GOIMPORT:
             parse_imports_decl();
+            parse_grammar();
+            break;
+        /* TEST RULE; BEGIN THIS */
+        case tk_CLASS:
+            printf("hello class\n");
+            token = gpplex();
             parse_grammar();
             break;
         case tk_IDENT:
